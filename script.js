49c459be2015a4b1b76e9bf1714d1f59d95e28cf
maliah hauser <maliah.hauser@stu.ocsb.ca>
2024-06-04 23:47:11 -0400

    did the first part of the website

---
+++ script.js
@@ -0,0 +1,11 @@
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
+
+window.onscroll = function(){
+    if (window.pageYOffset >= sticky){
+        navbar.classList.add("sticky");
+    }
+    else {
+        navbar.classList.remove("sticky");
+    }
+}
